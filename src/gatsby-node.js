0b0679230ecf0388fcef2eef8b80d5454ddf0095
Luiz Mai <lffmai@gmail.com>
2020-08-22 18:04:28 -0300

    Including gatsby-node file + Babel

---
+++ src/gatsby-node.js
@@ -0,0 +1,14 @@
+const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin")
+
+exports.onCreateWebpackConfig = ({ actions }, {pattern, input, output, sprite, styles}) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new SVGSpritemapPlugin(pattern, {
+        input,
+        output,
+        sprite,
+        styles
+      }),
+    ],
+  })
+}
